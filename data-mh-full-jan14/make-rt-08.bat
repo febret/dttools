dc263b2ac5e11ef843409fe5252d30424e341ff8
febret <febret@100823cb-c624-7a42-3e23-e1eaf87ca14b>
2014-01-26 01:46:37 +0000

    removed unused dir

--- data-mh-full-jan14/make-rt-08.bat
+++
@@ -1,2 +0,0 @@
-SET dtrt=X:/ENDURANCE/dttools/trunk/bin/win-x86-vs10-release/dtrt.exe 
-FOR %%D IN (05 07 08 09 10 12 13) DO %dtrt% bonney-08-dive%%D.dtrt.cfg
--- data-mh-full-jan14/make-rt-09.bat
+++
@@ -1,5 +0,0 @@
-SET dtrt=X:/ENDURANCE/dttools/trunk/bin/win-x86-vs10-release/dtrt.exe 
-
-
-FOR %%D IN (13 17 18 19 20 21 22 23 24 25 26 27) DO %dtrt% bonney-09-dive%%D.dtrt.cfg >>&2 bonney-dtrt.log
-
